45c23bc97311cf65b5256a440c1a2a3d156cfaac
flipott <ottphillipa@gmail.com>
2023-03-15 19:19:06 -0400

    Add functions for controlling mobile menu

---
+++ script.js
@@ -0,0 +1,20 @@
+const hamburgerIcon = document.getElementById("hamburger-icon");
+const mobileMenu = document.getElementById("mobile-menu");
+const mobileMenuLinks = document.getElementById("mobile-menu").children;
+
+hamburgerIcon.addEventListener("click", () => {
+    if (hamburgerIcon.classList.contains("show-mobile-menu")) {
+        hamburgerIcon.classList.remove("show-mobile-menu");
+        mobileMenu.classList.remove("show-mobile-menu");
+    } else {
+        hamburgerIcon.classList.add("show-mobile-menu");
+        mobileMenu.classList.add("show-mobile-menu");
+    }
+});
+
+for (let i = 0; i < mobileMenuLinks.length; i++) {
+    mobileMenuLinks[i].addEventListener("click", () => {
+        hamburgerIcon.classList.remove("show-mobile-menu");
+        mobileMenu.classList.remove("show-mobile-menu");
+    });
+}
